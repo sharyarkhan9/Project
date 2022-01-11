3b6d3b59c1f38e01b8862070f32aadb3675ccbcb
shryar khan <sharyar.khan@revature.net>
2022-01-11 15:16:08 -0500

    Adding project to gitHub

--- src/main/scala/Project0.scala
+++ src/main/scala/Project0.scala
@@ -3,6 +3,8 @@ import scala.collection.mutable.ArrayBuffer
 import java.util.Calendar
 import java.sql.DriverManager
 import java.sql.Connection
+import java.io.PrintWriter
+import java.io.File
 
 object Project0 {
     def main(arg:Array[String]): Unit = {   
@@ -10,66 +12,64 @@ object Project0 {
         val url = "jdbc:mysql://localhost:3306/project"
         val username = "root"
         val password = "F@ith6193" 
+        val log = new PrintWriter(new File("query.log"))
         var connection:Connection = null
         Class.forName(driver)
         connection = DriverManager.getConnection(url, username, password)
         val statement = connection.createStatement()   
         var scanner = new Scanner(System.in)
         var choice = 0
-        var arrBuff = new ArrayBuffer[String]()
-        while (choice != 4) {
-            println("Pleaes Choose an option using number keys 1 - 4: ")
-            println("1: Add a to-do list\n2: Remove a list\n3: Print all to do lists\n4: Update a to do list\n5: Exit")
+        while (choice != 5) {
+            println("Pleaes Choose an option using number keys 1 - 5: ")
+            println("1: Add a to-do list\n2: Remove a to-do list\n3: Print all to-do lists\n4: Update a to-do list\n5: Exit")
             try {
                 choice = scanner.nextInt()
                
             }catch {
-                case e: Exception => print("Exception has been thrown. A non numeric key was entered. ")
+                case e: Exception => print("Exception has been thrown. ")
             }
             scanner.nextLine()
                 if (choice == 1){
                     println("Please enter an item for you to do list:")
                     var item = scanner.nextLine()
-                    arrBuff += item
+                    var str2 = ""
+                    str2 += ("'"+item+"'")
+                    statement.executeUpdate("create table project0 (id int NOT NULL AUTO_INCREMENT, todo CHAR(255), PRIMARY KEY (id));")
+                    statement.executeUpdate("INSERT INTO project0 (todo) Values ("+str2+")")
+                    log.write(Calendar.getInstance().getTimeInMillis + ": Executing - INSERT INTO project0 (todo) Values ("+str2+")\n")
                 }
                 else if (choice ==2){
-                    arrBuff.foreach(println)
                     println("Please enter an id corresponding to the to do list you want removed:")
                     var item = scanner.nextInt()
                     statement.executeUpdate("DELETE FROM project0 WHERE id=("+item+")")
-                    log.write(Calendar.getInstance().getTimeInMillis + "- Executing - 'DELETE FROM project0 WHERE id=("+item+")'\n")
+                    log.write(Calendar.getInstance().getTimeInMillis + "- Executing - DELETE FROM project0 WHERE id=("+item+")\n")
                 }
                 else if (choice ==3){
                     val resultSet = statement.executeQuery("SELECT * FROM project0")
                     while ( resultSet.next() ) {
                         print(resultSet.getString(1) + " " + resultSet.getString(2))
                         println()
+                    log.write(Calendar.getInstance().getTimeInMillis + ": Executing - SELECT * FROM project0\n")
                     }
                 }
                 else if (choice ==4){
                     println("Please enter an id corresponding to the to do list you want to update:")
-                    var todo = scanner.nextLine()
-                    var str = ""
-                    str += ("'"+todo+"'")
-                    println("Please enter an id corresponding to the to do list you want to update:")
                     var item = scanner.nextInt()
+                    var str = ""
                     scanner.nextLine()
+                    println("Please enter the new to-do List: ")
+                    var todo = scanner.nextLine()
+                    str += ("'"+todo+"'")
                     statement.executeUpdate("update project0 set todo = ("+str+") where id =("+item+")")
+                    log.write(Calendar.getInstance().getTimeInMillis + ": Executing - update project0 set todo = ("+str+") where id =("+item+")\n")
               
                 }
 
                 else if (choice ==5){
-                    println("Your TO-DO list has been saved. Thank you")
-                    
-                    var str = ""
-                    for (x <- 0 until arrBuff.length) {
-                    var quer = ("'"+arrBuff(x)+"'")
-                    str += quer
-                    //statement.executeUpdate("create table project0 (id int NOT NULL AUTO_INCREMENT, todo CHAR(255), PRIMARY KEY (id));")
-                    statement.executeUpdate("INSERT INTO project0 (todo) Values ("+str+")")
-                    val resultSet = statement.executeQuery("SELECT * FROM project0")
-                } 
+                    println("Your TO-DO list has been saved. Thank you") 
                     connection.close()
+                    log.close()
+                    
                 }
                 else {
                     println("Please only enter numbers 1 - 4")
